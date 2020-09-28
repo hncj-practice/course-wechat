10ef170ce1b6e2866a692f33739c132d51b3f1a6
YuumiAndYasuo <1522426323@qq.com>
2020-09-28 13:56:54 +0800

    添加回复评论功能、修改密码功能、添加关于、登录页面背景图、话题详情页显示话题发布时间

--- CourseAssistant/pages/teacher/teacher-me/teacher-setting/teacher-setting.js
+++ CourseAssistant/pages/teacher/teacher-me/teacher-setting/teacher-setting.js
@@ -1,3 +1,5 @@
+const util = require("../../../../utils/util");
+
 // pages/teacher/teacher-me/teacher-setting/teacher-setting.js
 Page({
 
@@ -5,6 +7,76 @@ Page({
    * 页面的初始数据
    */
   data: {
+    changepwd:false,
+    about:false
+  },
+  //获取登录用户信息
+  isLogin() {
+    try {
+      var loginuser = wx.getStorageSync('loginuser');
+      console.log(loginuser)
+      if (loginuser) {
+        this.setData({
+          loginuser: loginuser
+        })
+      } else {
+        wx.showToast({
+          title: '未登录，请登录后重试',
+          icon: 'none',
+          duration: 3000
+        })
+
+        setTimeout(function () {
+          wx.navigateTo({
+            url: '../../login/login',
+          })
+        }, 3000);
+
+      }
+    } catch (e) {}
+  },
+  //显示修改模态框
+  showModify(){
+    this.setData({
+      changepwd:!this.data.changepwd
+    })
+  },
+  showAbout(){
+    this.setData({
+      about:!this.data.about
+    })
+  },
+  //修改密码
+  modifyPwd(event){
+    var that=this;
+    var loginuser=that.data.loginuser;
+    //获取工号
+    var tno=loginuser.tno;
+    //获取旧密码和新密码
+    var olderpwd=event.detail.value.olderpwd;
+    var newpwd=event.detail.value.newpwd;
+
+    var url="https://fengyezhan.xyz/Interface/account/resetpwd";
+    var data={
+      username:tno,
+      password:olderpwd,
+      newpwd:newpwd,
+      type:2
+    }
+    util.myAjaxPost(url,data).then(res=>{
+      wx.showToast({
+        title: res.data.message,
+        icon: 'none'
+      })
+      if (res.data.code != 200) {
+        return;
+      }
+      setTimeout(function(){
+        that.showModify();
+        that.logout();
+      },1000)
+    })
+
 
   },
 
@@ -12,9 +84,6 @@ Page({
    * 退出登录
    */
   logout() {
-    // wx.redirectTo({
-    //   url: '../../../login/login',
-    // });
     wx.reLaunch({
       url: '../../../login/login',
     })
@@ -24,7 +93,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.isLogin();
   },
 
   /**
