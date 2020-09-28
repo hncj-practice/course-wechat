a4d2bf623476a2da7619685ba1147cdc18bb1f8c
Tanyiqu <1953649096@qq.com>
2020-09-28 15:51:12 +0800

    学生交卷操作 初步

--- CourseAssistant/pages/student/student-msg/student-paper/student-paper.js
+++ CourseAssistant/pages/student/student-msg/student-paper/student-paper.js
@@ -8,11 +8,55 @@ Page({
 
   },
 
+  // 做题前警告
+  showWarning() {
+    wx.showModal({
+      title: '警告',
+      content: '请不要再答题过程中离开此页面！！',
+      success: (res) => {
+        if (res.confirm) {
+          console.log('开始做题');
+
+          // 请求数据
+          // ajax();
+
+
+        } else {
+          console.log('返回');
+          wx.navigateBack();
+        }
+      }
+    });
+  },
+
+  // 点击交卷
+  onSubmitPaper() {
+    console.log('点击交卷');
+    wx.showModal({
+      title: '警告',
+      content: '请确认答题完毕再点击交卷！！',
+      success: (res) => {
+        if (res.confirm) {
+          this.submitPaper();
+        } else {
+          console.log('取消');
+        }
+      }
+    })
+  },
+
+
+  // 交卷
+  submitPaper() {
+    console.log('提交试卷');
+
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.showWarning();
   },
 
   /**
