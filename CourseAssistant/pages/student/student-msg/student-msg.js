2241c691a8203a305182ed52040bee7d1ac80b00
Tanyiqu <1953649096@qq.com>
2020-09-28 09:42:56 +0800

    学生通知页面

--- CourseAssistant/pages/student/student-msg/student-msg.js
+++ CourseAssistant/pages/student/student-msg/student-msg.js
@@ -8,20 +8,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    showGoing:true,
-    showEnded:true
+    showGoing: true,
+    showEnded: true
   },
 
 
 
-// 切换折叠菜单
-trigger(e){
-  console.log('切换');
+  // 切换折叠菜单
+  trigger(e) {
+    let log = e.currentTarget.dataset.tag;
+    if (log === 'going') {
+      this.setData({
+        showGoing: !this.data.showGoing
+      });
+    } else {
+      this.setData({
+        showEnded: !this.data.showEnded
+      });
+    }
 
-  
-  
-  
-},
+  },
 
   // tabbar跳转
   jump(e) {
@@ -32,56 +38,56 @@ trigger(e){
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function(options) {
+  onLoad: function (options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function() {
+  onReady: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function() {
+  onShow: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function() {
+  onHide: function () {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function() {
+  onUnload: function () {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function() {
+  onPullDownRefresh: function () {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function() {
+  onReachBottom: function () {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function () {
 
   }
 })
